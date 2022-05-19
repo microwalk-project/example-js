1cba70530465f9edf9f41a46b2652903b4d7065c
Jan Wichelmann <jan.wichelmann@hotmail.de>
2022-05-19 23:48:46 +0200

    Add leakage

--- functions.js
+++ functions.js
@@ -1,12 +1,18 @@
 
+var lookup = [ 0, 1, 2, 3, 4, 5, 6, 7 ];
+
 function function1(x)
 {
-	return x;
+	return lookup[x % 8];
 }
 
 function function2(x)
 {
-	return x;
+	var sum = 0;
+	for(var i = 0; i < x; i++)
+		sum += 1;
+		
+	return sum;
 }
 
 module.exports = { function1, function2 };
